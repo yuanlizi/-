8d65ff29c1943624d4459e5fccd94fcc1bed1197
LiYuanLiyuanLiiiii <1347335802@qq.com>
2022-11-02 17:59:22 +0800

    init

---
+++ src/views/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>About</div>
+</template>
+
+<script lang='ts'>
+import {defineComponent, reactive, toRefs, onBeforeMount, onMounted } from 'vue'
+export default defineComponent({
+  name: '',
+  setup() {
+    const data = reactive({})
+    onBeforeMount(() => {
+     //2.组件挂载页面之前执行----onBeforeMount
+    })
+    onMounted(() => {
+      //3.组件挂载到页面之后执行-------onMounted
+    })
+    return {
+      ...toRefs(data),
+    }
+  },
+})
+
+</script>
+<style scoped lang='less'>
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>Home</div>
+  <list/>
+</template>
+
+<script lang='ts'>
+import {defineComponent, reactive, toRefs, onBeforeMount, onMounted } from 'vue'
+export default defineComponent({
+  name: '',
+  setup() {
+    const data = reactive({})
+    onBeforeMount(() => {
+     //2.组件挂载页面之前执行----onBeforeMount
+    })
+    onMounted(() => {
+      //3.组件挂载到页面之后执行-------onMounted
+    })
+    return {
+      ...toRefs(data),
+    }
+  },
+})
+
+</script>
+<style scoped lang='less'>
+</style>
